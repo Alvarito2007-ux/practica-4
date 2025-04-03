2049b7e0bae1cf54b9cac12f58daa8b8aacf01e8
Alvarito2007-ux <alvaromadro3@gmail.com>
2025-04-03 09:47:57 +0200

    2º trimetre pt1

--- database/connection.js
+++ database/connection.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 const connection = async () => {
 
     try { 
-        await mongoose.connect("mongodb://localhost:27017/mi_blog");
+        await mongoose.connect("mongodb+srv://username_blog_uer:Z4lpjaUYbMrIg0uq@cluster0.jjk3i.mongodb.net/mi_blog");
         console.log("Conectado correctamente a la base de datos mi_blog!!");
 
     }
